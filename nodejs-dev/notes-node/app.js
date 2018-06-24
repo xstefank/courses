c148b5c14406956ac09eafa723c447b42756b8d4
xstefank <xstefank122@gmail.com>
2018-06-24 16:14:52 +0200

    Add node-dev notes-node - external file require

--- nodejs-dev/notes-node/app.js
+++ nodejs-dev/notes-node/app.js
@@ -1,13 +1,20 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
 
+var sum = notes.add(9, -2);
+console.log(sum);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// var user = os.userInfo();
+//
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
