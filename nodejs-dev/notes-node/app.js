02fdee46e2a20f1c94eb12a52dbcf215d2f5d7cd
xstefank <xstefank122@gmail.com>
2018-06-24 10:24:09 +0200

    Add node-dev notes-node

---
+++ nodejs-dev/notes-node/app.js
@@ -0,0 +1,13 @@
+console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+  if (err) {
+    console.log('Unable to write to file');
+  }
+});
