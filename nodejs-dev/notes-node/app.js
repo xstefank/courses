9d515cf9496e040ca199133d8432b336545db1c5
xstefank <xstefank122@gmail.com>
2018-06-27 23:58:33 +0200

    Add json persisting to notes-node

--- nodejs-dev/notes-node/app.js
+++ nodejs-dev/notes-node/app.js
@@ -7,8 +7,8 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
-console.log('Command: ', command);
+var command = argv._[0];
+console.log('Command:', command);
 console.log('Yargs', argv);
 
 if (command === 'add') {
