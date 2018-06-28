7c8f7a48c769a667ad26d38fbf6580f664b51999
xstefank <xstefank122@gmail.com>
2018-06-28 21:15:58 +0200

    Add note reading to notes-node

--- nodejs-dev/notes-node/notes.js
+++ nodejs-dev/notes-node/notes.js
@@ -36,7 +36,9 @@ var getAll = () => {
 }
 
 var getNote = (title) => {
-  console.log('Getting note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 }
 
 var removeNote = (title) => {
@@ -47,9 +49,16 @@ var removeNote = (title) => {
   return notes.length !== removedNotes.length;
 }
 
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+}
+
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
