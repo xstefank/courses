610f27b0a048a0df5716d9b50b7e3a298585a637
xstefank <xstefank122@gmail.com>
2018-07-01 23:28:08 +0200

    Add weather api calls to weather-app

--- nodejs-dev/weather-app/app.js
+++ nodejs-dev/weather-app/app.js
@@ -1,32 +1,37 @@
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
 const request = require('request');
-const yargs = require('yargs');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  url: 'https://api.darksky.net/forecast/e5932b5ab6f99c06b723286c161f942d/39.9444071,-75.1631718',
   json: true
 }, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect ot Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather');
   }
 });
